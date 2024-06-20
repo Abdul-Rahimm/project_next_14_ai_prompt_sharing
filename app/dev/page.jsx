ca133bedf2e1273ffbd97b4912894da7cf0022c2
AbdulRaheemx <abdulraheemar611@gmail.com>
2024-06-21 00:23:46 +0500

    created new branch dev

---
+++ app/dev/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dev = () => {
+  return (
+    <div>
+      <h1 className="head_text blue_gradient">Development</h1>
+    </div>
+  );
+};
+
+export default Dev;
